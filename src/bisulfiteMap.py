6ff30737d985b7222a131bca285cbff1db448a70
aowen87 <alisterowen@hotmail.com>
2016-04-25 15:23:54 -0700

    commit

--- src/bisulfiteMap.py
+++ src/bisulfiteMap.py
@@ -35,7 +35,7 @@ def bisulfiteMap(genome, fastFile, result_directory, build, m):
         os.system('build_bw -P {}'.format(genome))
         os.system('build_bw -P {} -G 2 -r {}'.format(genome, genome)) 
     os.system('trim -s {} -P N5417 -q 20 -m {}'.format(fastFile, m))
-    os.system('brat_bw -P Nc12_genome -s BStrim.txt -o BSmapped.txt -W -C -m {}'.format(m))
+    os.system('brat_bw -P Nc12_genome -s BStrim.txt -o BSmapped.txt -W -C -m {}'.format(m)) #FIXME: genome hard coded
     os.system('remove-dupl -r {}.txt -s BSmapped.txt'.format(genome)) #genome.txt?????
     os.system('argt-count -r {} -P 5mC_BSmapped -s BSmapped_rd.txt -B'.format(genome))
     
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     parser.add_argument('genome', type=str, help='a complete genome to map reads to')
     parser.add_argument('fastFile', type=str, help='a fastq file containing bisulfite-seq reads')
     parser.add_argument('result_directory', type=str, help='the directory where the (entire) pipeline results will be stored')
-    parser.add_argument('build', type=bool, action='store_false', help='Do you need to build a genome? default=False')
+    parser.add_argument('build', action='store_false', help='Do you need to build a genome? default=False')
     parser.add_argument('m', type=int, nargs='?', default=2, help='specify the number of non-BS mismatches. default=2')
     args = parser.parse_args()
     genome = args.genome
