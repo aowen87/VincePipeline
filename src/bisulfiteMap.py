0e4bbf5047802898d8aa952dcd120da1b0249970
aowen87 <alisterowen@hotmail.com>
2016-04-29 17:21:53 -0700

    commit

--- src/bisulfiteMap.py
+++ src/bisulfiteMap.py
@@ -9,14 +9,14 @@ bisulfite-seq reads.
 
 @author: alister
 '''
-
 import os
 import sys
 import argparse
 import glob
+from collections import defaultdict
 
 
-def bisulfiteMap(BRAT_genome_dir, fastq_dir, strand_name, result_dir, build, non_BS_mismatches, quality_score):  
+def bisulfiteMap(BRAT_genome_dir, fastq_dir, result_dir, build, non_BS_mismatches, quality_score):  
     '''
     Run BRAT-BW to map bisulfite-seq reads.
     args: BRAT_genome_dir (a reference BRAT_genome_dir -> str)
@@ -44,41 +44,43 @@ def bisulfiteMap(BRAT_genome_dir, fastq_dir, strand_name, result_dir, build, non
         os.system('build_bw -P {}'.format(BRAT_genome_dir))
         os.system('build_bw -P {} -G 2 -r {}'.format(BRAT_genome_dir, BRAT_genome_dir)) 
     count = len(fastqFiles)
+    strand_count = defaultdict(int)
     for i in range(count):
-        os.system('trim -s {} -P {} -q {} -m {}'.format(fastqFiles[i], strand_name, quality_score, non_BS_mismatches))
-        os.system('brat_bw -P {} -s {}_reads1.txt -o BSmapped.txt -W -C -m {}'.format(BRAT_genome_dir, strand_name, non_BS_mismatches)) 
-        os.system('remove-dupl -r Nc12genome -s BSmapped.txt'.format(BRAT_genome_dir))                      #FIXME: BRAT_genome_dir file hard coded. change to param?
-        os.system('acgt-count -r Nc12genome -P 5mC_BSmapped -s 5mC_BSmapped_forw.txt -B')                     # same as above ^
-        os.system('acgt-count -r Nc12genome -P 5mC_BSmapped -s 5mC_BSmapped_rev.txt -B')                      # same as above ^
+        strand = fastqFiles[i].split("_")[1]
+        strand_count[strand] += 1
+        os.system('trim -s {} -P {}_{} -q {} -m {}'.format(fastqFiles[i], strand, strand_count[strand], quality_score, non_BS_mismatches))
+        os.system('brat_bw -P {} -s {}_{}_reads1.txt -o BSmapped.txt -W -C -m {}'.format(BRAT_genome_dir, strand, strand_count[strand], non_BS_mismatches)) 
+        os.system('remove-dupl -r Nc12genome -s BSmapped.txt'.format(BRAT_genome_dir))        #FIXME: BRAT_genome_dir file hard coded. change to param?
+        os.system('acgt-count -r Nc12genome -P 5mC_BSmapped -s 5mC_BSmapped_forw.txt -B')           # same as above ^
+        os.system('acgt-count -r Nc12genome -P 5mC_BSmapped -s 5mC_BSmapped_rev.txt -B')            # same as above ^
         os.system('mv 5mC_BSmapped_forw.txt 5mC_BSmapped_forw_{}.txt'.format(i))  
         os.system('mv 5mC_BSmapped_rev.txt 5mC_BSmapped_rev_{}.txt'.format(i))
         os.system('mv 5mC_BSmapped_forw_{}.txt {}'.format(i, result_dir))    
         os.system('mv 5mC_BSmapped_rev_{}.txt {}'.format(i, result_dir)) 
         #NOTE: may need to alter other file names if they are needed further down the pipeline.
 
+
 if __name__ == "__main__":
     '''
     Set default values for various parameters. Run from the command line. 
     '''
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 3:
         print("""Usage:  {} <BRAT_genome_dir> <fasq_dir> <strand_name> <result_dir> <build=False> 
         <non_BS_mistmatches=2> <quality_score=20>""".format(sys.argv[0]))
         exit(1)
     parser = argparse.ArgumentParser(description="run BRAT-BW to map bisulfite-seq reads")
     parser.add_argument('BRAT_genome', type=str, help='a BRAT-BW genome directory')
     parser.add_argument('fastq_dir', type=str, help='a directory containing fastq files of bisulfite-seq reads')
-    parser.add_argument('strand_name', type=str, help='the name of the strand')
     parser.add_argument('result_dir', type=str, help='the directory where the protocol results will be stored')
     parser.add_argument('build', action='store_false', help='Do you need to build a genome? default=False')
     parser.add_argument('non_BS_mismatches', type=int, nargs='?', default=2, help='specify the number of non-BS mismatches. default=2')
     parser.add_argument('quality_score', type=int, nargs='?', default=20, help='quality score. default=20')
     args = parser.parse_args()
     genome = args.BRAT_genome
-    strand = args.strand_name
     fastFile = args.fastq_dir
     result_dir = args.result_dir
     build = args.build
     m = args.non_BS_mismatches
     qs = args.quality_score
-    bisulfiteMap(genome, fastFile, strand, result_dir, build, m, qs)
+    bisulfiteMap(genome, fastFile, result_dir, build, m, qs)
 
