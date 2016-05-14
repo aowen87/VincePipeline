43c711634f1aa342b2a5868e02ac76afab0b544f
Hiranmayi Duvvuri <hiranmayid8@gmail.com>
2016-05-14 15:46:59 -0700

    fixing inputs

--- src/methylome_comp.py
+++ src/methylome_comp.py
@@ -7,12 +7,12 @@ Compare methylomes and return the differential methylation scores between the tw
 import os, sys, glob, argparse
 
 # compare mutants to a control/wildtype
-def diff_comp(input_dir, output_dir, compare_all = True, wt = None, mut = None):
+def diff_comp(input_dir, output_dir, wt_meth = None, wt_hmr = None):
     """
     begin running Meth-Pipe on converted meth-pipe file. 
     and look at the differential methylation scores between two samples. 
     args:
-        input_dir: files to compare control to
+        input_dir: .hmr and .meth files to compare control to
         output_dir: location to save comparisons
         compare_all: compare all .meth files if True
         wt: wildtype or control file
@@ -23,52 +23,38 @@ def diff_comp(input_dir, output_dir, compare_all = True, wt = None, mut = None):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    mut_files = [x for x in glob.glob('{}/*.meth'.format(input_dir))]
+    mut_meth = [x for x in glob.glob('{}/*.meth'.format(input_dir))]
+    mut_hmr[x for x in glob.glob('{}/*.hmr'.format(input_dir))]
 
-    if compare_all and wt != None:
-        for i in mut_files:
-            s1 = wt.split('/')[-1].split('_')[1]
+    if compare_all and wt_meth != None and wt_hmr != None:
+        for i, j in zip(mut_meth, mut_hmr):
+            s1 = wt_meth.split('/')[-1].split('_')[1]
             s2 = i.split('/')[-1].split('_')[1]
 
-            os.system("methdiff -o {}/{}vs{}.methdiff {} {}".format(output_dir, s1, s2, wt, j))
-            os.system("dmr {}/{}vs{}.methdiff {}/{}_inverted.hmr {}/{}_inverted.hmr {}/DMR_{}vs{}_lt_{} {}/DMR_{}vs{}_lt_{}".format(output_dir, s1,
-                s2, output_dir, s1, output_dir, s2, output_dir, s1, s2, s1, output_dir, s1, s2, s2))
-    else:
-        if wt and mut != None:
-            s1 = wt.split('/')[-1].split('_')[1]
-            s2 = mut.split('/')[-1].split('_')[1]
+            print('finding differential methylation scores between ' + s1 + ' and ' + s2 + '...')
 
-            os.system("methdiff -o {}/{}vs{}.methdiff {} {}".format(output_dir, s1, s2, wt, mut))
-            os.system("dmr {}/{}vs{}.methdiff {}/{}_inverted.hmr {}/{}_inverted.hmr {}/DMR_{}vs{}_lt_{} {}/DMR_{}vs{}_lt_{}".format(output_dir, s1,
-                s2, output_dir, s1, output_dir, s2, output_dir, s1, s2, s1, output_dir, s1, s2, s2))
-        else:
-            ValueError("please enter two files to compare")
+            os.system("methdiff -o {}/{}vs{}.methdiff {} {}".format(output_dir, s1, s2, wt_meth, i))
+            os.system("dmr {}/{}vs{}.methdiff {} {} {}/DMR_{}vs{}_lt_{} {}/DMR_{}vs{}_lt_{}".format(output_dir, s1,
+                        s2, wt_hmr, j, output_dir, s1, s2, s1, output_dir, s1, s2, s2))
+    else:
+        raise ValueError("please enter a")
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description = 'Compare methylomes to a control/wild type sample')
     parser.add_argument("mut_input", 
-                        nargs = '?',
                         type = str, 
-                        help = "directory containing files to compare to control",
-                        default = None)
+                        help = "directory containing files to compare to control")
     parser.add_argument("comp_dir", 
-                        nargs = '?',
                         type = str, 
-                        help = "output directory for differential methylation scores",
+                        help = "output directory for differential methylation scores")
+    parser.add_argument("wt_meth",
+                        help = "control .meth file", 
                         default = None)
-    parser.add_argument("-c" "compare_all", 
-                        nargs = '?',
-                        help = "if True, compare all .meth files in directory. If False, only compare given files", 
-                        default = True)
-    parser.add_argument("wt",
-                        help = "first of two .meth files to compare", 
-                        default = None)
-    parser.add_argument("mut", 
-                        nargs = '?',
-                        help = "Only required", 
+    parser.add_argument("wt_hmr", 
+                        help = "control .hmr file", 
                         default = None)
 
     args = parser.parse_args()
 
-    diff_comp(args.meth_pipe_dir, args.comp_dir, args.compare_all, args.sample_1, args.sample_2)
+    diff_comp(args.mut_input, args.comp_dir, args.wt_meth, args.wt_hmr)
