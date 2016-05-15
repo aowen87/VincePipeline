3ddc945466fc999977d15020e26fbbc744930762
aowen87 <alisterowen@hotmail.com>
2016-05-15 16:39:05 -0700

    updates for analyzing

--- src/analyzing.py
+++ src/analyzing.py
@@ -1,4 +1,3 @@
-__author__ = 'xixi'
 '''
 Created on Apr 24, 2016
 Moldified on Mat 6, 2016
@@ -13,7 +12,7 @@ import argparse
 import glob
 
 
-def stepAnalyzing(file_dir, result_dir):
+def stepAnalyzing(file_dir, result_dir, clean):
     """
     The step for analyzing the final BRAT ACGT count 5mC output files.
     """
@@ -25,8 +24,16 @@ def stepAnalyzing(file_dir, result_dir):
             sys.exit("\nEXITING\n")
     else:
         os.system('mkdir {}'.format(result_dir))
+        
+    avgPath = result_dir + '/5mCAverages'
+    if not os.path.isdir(avgPath):
+        os.system('mkdir {}'.format(avgPath))
+        
+    wigPath = result_dir + '/wigFiles'
+    if not os.path.isdir(wigPath):
+        os.system('mkdir {}'.format(wigPath))
 
-    mCfiles = [file for file in glob.glob('./{}/*.txt'.format(file_dir))]
+    mCfiles  = [file for file in glob.glob('./{}/*.txt'.format(file_dir))]
     mCfiles.sort()
     file_len = len(mCfiles)
     if file_len%2!=0:
@@ -35,7 +42,11 @@ def stepAnalyzing(file_dir, result_dir):
     for i in range(0, file_len, 2):
         forw = mCfiles[i]
         rev  = mCfiles[i+1]
-
+        fileID = forw.split('_')[2] + '_' + forw.split('_')[3]
+        avgFile = avgPath + '/' + fileID
+        wigFile = wigPath + '/' + fileID + '.wig'
+        os.system('touch {}'.format(avgFile))
+        
         unProcMerge = rev.replace("rev", "unProcMerge").replace(file_dir, result_dir)
         merged      = rev.replace("rev", "merged").replace(file_dir, result_dir)
         sort        = rev.replace("rev", "sort").replace(file_dir, result_dir)
@@ -45,29 +56,32 @@ def stepAnalyzing(file_dir, result_dir):
         CHG         = rev.replace("rev", "CHG").replace(file_dir, result_dir)
         # extract forw, rev file, and generate corresponding file names
          
-         
-        #FIXME: there are several print commands that may be unnecesarry (they aren't affecting 
-        #any of the output files). We should ask Vince if these are extraneous. 
         os.system('cat {} {} > {}'.format(forw, rev, unProcMerge))
         os.system("sed 's/:/\t/g' {} > {}".format(unProcMerge, merged))
         os.system('sort {} -o {}'.format(merged, sort))
-        os.system("""awk '{sum+=$6} END {print "Average for " FILENAME "= ",sum/NR}' """ + sort)
-        os.system("awk 'END {{print NR}}' {}".format(sort))
+        os.system("""awk '{{sum+=$6}} END {{print "Average level of 5mC for {} = ",sum/NR}}' {} > {}""".format(fileID, sort,  avgFile))
+        os.system("""awk 'END {{print "total cytosines: ", NR}}' {} >> {}""".format(sort, avgFile))
         os.system("awk '$6>0' {} > {}".format(sort, mC))
-        os.system("""awk '{sum+=$6} END {print "Average for " FILENAME "= ",sum/NR}' """ + mC)
-        os.system("""awk '{sum+=$5} END {print "Average reads for " FILENAME "= ",sum/NR}' """ + sort)
+        os.system("""awk '{{sum+=$6}} END {{print "Average for {} with methyl level > 0 = ",sum/NR}}' {}  >> {}""".format(fileID, mC, avgFile))
+        os.system("""awk '{{sum+=$5}} END {{print "Average reads for {} = ",sum/NR}}' {} >> {}""".format(fileID, sort, avgFile))
         
         os.system('grep CHH {} > {} '.format(sort,CHH))
         os.system('grep CG {} > {}'.format(sort, CG))
         os.system('grep CHG {} > {}'.format(sort, CHG))
         
-        os.system("""awk '{sum+=$6} END {print "Average for " FILENAME "= ",sum/NR}' """ + CHH)
-        os.system("""awk '{sum+=$6} END {print "Average for " FILENAME "= ",sum/NR}' """ + CG)
-        os.system("""awk '{sum+=$6} END {print "Average for " FILENAME "= ",sum/NR}' """ + CHG)
+        os.system("""awk '{{sum+=$6}} END {{print "Average 5mC for {} = ",sum/NR}}' {} >> {}""".format(fileID, CHH, avgFile))
+        os.system("""awk '{{sum+=$6}} END {{print "Average 5mC for {} = ",sum/NR}}' {} >> {}""".format(fileID, CG, avgFile))
+        os.system("""awk '{{sum+=$6}} END {{print "Average 5mC for {} = ",sum/NR}}' {} >> {}""".format(fileID, CHG, avgFile))
+       
+
+        os.system("""awk 'END {{print "total CHH: ", NR }}' {} >> {}""".format(CHH, avgFile))
+        os.system("""awk 'END {{print "total CG: ", NR }}' {} >> {}""".format(CG, avgFile))
+        os.system("""awk 'END {{print "total CHG: ", NR }}' {} >> {}""".format(CHG, avgFile))
+    
+        os.system('cut -f 1,2,3,6 {} > {}'.format(mC, wigFile))
         
-        os.system("awk 'END {{print NR }}' {}".format(CHH))
-        os.system("awk 'END {{print NR }}' {}".format(CG))
-        os.system("awk 'END {{print NR }}' {}".format(CHG))
+    if clean:
+        os.system('rm {}/*.txt'.format(result_dir))
         
 
 if __name__ == "__main__":
@@ -80,7 +94,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="analysis for 5mC files")
     parser.add_argument("FILE_dir", type=str, help="directory folder contains source files")
     parser.add_argument("RESULT_dir", type=str, help="directory folder to store result files")
+    parser.add_argument('clean', action='store_true', help="""Would you like to remove all 
+                         files ouput files not used further on?""")
     args = parser.parse_args()
     file = args.FILE_dir
     result = args.RESULT_dir
-    stepAnalyzing(file, result)
+    clean = args.clean
+    stepAnalyzing(file, result, clean)
--- src/bisulfiteMap.py
+++ src/bisulfiteMap.py
@@ -16,7 +16,7 @@ import glob
 from collections import defaultdict
 
 
-def bisulfiteMap(BRAT_genome_dir, fastq_dir, result_dir, build, non_BS_mismatches, quality_score):  
+def bisulfiteMap(BRAT_genome_dir, fastq_dir, result_dir, clean, build, non_BS_mismatches, quality_score):  
     '''
     Run BRAT-BW to map bisulfite-seq reads.
     args: BRAT_genome_dir (a reference BRAT_genome_dir -> str)
@@ -26,18 +26,22 @@ def bisulfiteMap(BRAT_genome_dir, fastq_dir, result_dir, build, non_BS_mismatche
           non_BS_mismatches (the number of non-BS mismatches -> int)
           quality_score (the quality score -> int)
     '''
-    
+       
     if os.path.isdir(result_dir):
         print("\n{} already exists...".format(result_dir))
         cont = input("\nUsing this directory may result in current files being replaced. Would you like to continue? (y/n) ")
         if cont != 'y':
             sys.exit("\nEXITING\n")
+            
+    if not os.path.isfile("mappedDupl"):
+        os.system("touch mappedDupl")
+        os.system("printf BSmapped.txt > mappedDupl")
+        
+    if not os.path.isfile("mappedNoDupl"):
+        os.system("touch mappedNoDupl")
+        os.system("printf BSmapped.txt.nodupl > mappedDupl")
     
-    #FIXME:
-    #check to see if file 'mappedDupl' exists. If not, create it and add 
-    #the text 'BSmapped.txt'. 
-    #Check to see if 'mappedNoDupl' exists. If not, create it and add 
-    #the text 'BSmapped.txt.nodupl'.
+    #FIXME: option to remove all created files other than the wig file.  
     else:
         os.system('mkdir {}'.format(result_dir))
     
@@ -58,7 +62,14 @@ def bisulfiteMap(BRAT_genome_dir, fastq_dir, result_dir, build, non_BS_mismatche
         os.system('mv 5mC_BSmapped_N{}_{}_forw.txt {}'.format(strand, strand_count[strand], result_dir))    
         os.system('mv 5mC_BSmapped_N{}_{}_rev.txt {}'.format(strand, strand_count[strand], result_dir)) 
         os.system('rm *.nodupl')
-        #NOTE: may need to alter other file names if they are needed further down the pipeline.
+        
+    if clean:
+        os.system('rm *.txt')
+    else:
+        if not os.path.isdir('otherOutput'):
+            os.system('mkdir otherOutput')
+        os.system('mv *.txt otherOutput')
+       
         
 if __name__ == "__main__":
     '''
@@ -72,6 +83,7 @@ if __name__ == "__main__":
     parser.add_argument('BRAT_genome', type=str, help='a BRAT-BW genome directory')
     parser.add_argument('fastq_dir', type=str, help='a directory containing fastq files of bisulfite-seq reads')
     parser.add_argument('result_dir', type=str, help='the directory where the protocol results will be stored')
+    parser.add_argument('clean', action='store_false', help='Would you like to remove all files ouput files not used further on?')
     parser.add_argument('build', action='store_false', help='Do you need to build a genome? default=False')
     parser.add_argument('non_BS_mismatches', type=int, nargs='?', default=2, help='specify the number of non-BS mismatches. default=2')
     parser.add_argument('quality_score', type=int, nargs='?', default=20, help='quality score. default=20')
@@ -79,8 +91,9 @@ if __name__ == "__main__":
     genome = args.BRAT_genome
     fastFile = args.fastq_dir
     result_dir = args.result_dir
+    clean = args.clean
     build = args.build
     m = args.non_BS_mismatches
     qs = args.quality_score
-    bisulfiteMap(genome, fastFile, result_dir, build, m, qs)
+    bisulfiteMap(genome, fastFile, result_dir, clean, build, m, qs)
 
