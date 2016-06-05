d707bfb4c6bb566c1f35b10332bd324ceddc8c51
aowen87 <alisterowen@hotmail.com>
2016-06-05 16:43:50 -0700

    updates

--- src/pipeline/analyzing.py
+++ src/pipeline/analyzing.py
@@ -1,60 +1,64 @@
-'''
-Created on Apr 24, 2016
-Moldified on Mat 6, 2016
-The first in a sequence of functions designed
+__author__ = 'Alister Maguire, xi'
+__date__ = '04/24/2016'
+__description__ = """The first in a sequence of functions designed
 to pipeline Mike Roundtree's BS-seq protocol.
-Generated statistics for 5mC level, CHH, CG, CHG files.
-@author: alister, xi
-'''
+Generated statistics for 5mC level, CHH, CG, CHG files. """
+
 import os
 import sys
 import argparse
 import glob
 
 
-def stepAnalyzing(file_dir, result_dir, clean):
+
+def stepAnalyzing(fileDir, resultsDir, clean):
     """
     The step for analyzing the final BRAT ACGT count 5mC output files.
-    args: file_dir (a directory containing 5mC forward and reverse files -> str)
-          result_dir (a target directory to move results into -> str)
+    args: fileDir (a directory containing 5mC forward and reverse files -> str)
+          resultsDir (a target directory to move results into -> str)
           clean (clean up extraneous output -> boolean) 
     """
 
-    if not os.path.isdir(result_dir):
-        os.system('mkdir {}'.format(result_dir))
+    if not os.path.isdir(resultsDir):
+        os.system('mkdir {}'.format(resultsDir))
         
-    avgPath = result_dir + '/5mCAverages'
+    avgPath = resultsDir + '/5mCAverages'
     if not os.path.isdir(avgPath):
         os.system('mkdir {}'.format(avgPath))
         
-    wigPath = result_dir + '/wigFiles'
+    wigPath = resultsDir + '/wigFiles'
     if not os.path.isdir(wigPath):
         os.system('mkdir {}'.format(wigPath))
+        
+    mergedPath = resultsDir + '/mergedFiles'
+    if not os.path.isdir(mergedPath):
+        os.system('mkdir {}'.format(mergedPath))
 
-    mCfiles  = [file for file in glob.glob('./{}/*.txt'.format(file_dir))]
+    mCfiles = [fileDir for fileDir in glob.glob('./{}/*.txt'.format(fileDir))]
     mCfiles.sort()
-    file_len = len(mCfiles)
-    if file_len%2!=0:
-        file_len-=1
+    fileLen = len(mCfiles)
+    if fileLen%2!=0:
+        fileLen-=1
 
-    for i in range(0, file_len, 2):
-        forw = mCfiles[i]
-        rev  = mCfiles[i+1]
-        fileID = forw.split('_')[2] + '_' + forw.split('_')[3]
+    for i in range(0, fileLen, 2):
+        forw    = mCfiles[i]
+        rev     = mCfiles[i+1]
+        fileID  = forw.split('_')[2] + '_' + forw.split('_')[3]
         avgFile = avgPath + '/' + fileID
         wigFile = wigPath + '/' + fileID + '.wig'
         os.system('touch {}'.format(avgFile))
         
-        unProcMerge = rev.replace("rev", "unProcMerge").replace(file_dir, result_dir)
-        merged      = rev.replace("rev", "merged").replace(file_dir, result_dir)
-        sort        = rev.replace("rev", "sort").replace(file_dir, result_dir)
-        mC          = rev.replace("rev", "only5mC").replace(file_dir, result_dir)
-        CHH         = rev.replace("rev", "CHH").replace(file_dir, result_dir)
-        CG          = rev.replace("rev", "CG").replace(file_dir, result_dir)
-        CHG         = rev.replace("rev", "CHG").replace(file_dir, result_dir)
+        unProcMerge = rev.replace("rev", "unProcMerge").replace(fileDir, resultsDir)
+        merged      = rev.replace("rev", "merged").replace(fileDir, resultsDir)
+        sort        = rev.replace("rev", "sort").replace(fileDir, resultsDir)
+        mC          = rev.replace("rev", "only5mC").replace(fileDir, resultsDir)
+        CHH         = rev.replace("rev", "CHH").replace(fileDir, resultsDir)
+        CG          = rev.replace("rev", "CG").replace(fileDir, resultsDir)
+        CHG         = rev.replace("rev", "CHG").replace(fileDir, resultsDir)
          
         os.system('cat {} {} > {}'.format(forw, rev, unProcMerge))
         os.system("sed 's/:/\t/g' {} > {}".format(unProcMerge, merged))
+        os.system('cp {} {}'.format(merged, mergedPath))
         os.system('sort {} -o {}'.format(merged, sort))
         os.system("""awk '{{sum+=$6}} END {{print "Average level of 5mC for {} = ",sum/NR}}' {} > {}""".format(fileID, sort,  avgFile))
         os.system("""awk 'END {{print "total cytosines: ", NR}}' {} >> {}""".format(sort, avgFile))
@@ -78,7 +82,7 @@ def stepAnalyzing(file_dir, result_dir, clean):
         os.system('cut -f 1,2,3,6 {} > {}'.format(mC, wigFile))
         
     if clean:
-        os.system('rm {}/*.txt'.format(result_dir))
+        os.system('rm {}/*.txt'.format(resultsDir))
         os.system('rm *.txt')
         os.system('rm *.fastq')
     else:
@@ -93,16 +97,20 @@ if __name__ == "__main__":
     Set default values for various parameters. Run from the command line.
     '''
     if len(sys.argv) < 2:  
-        print("""Usage:  {} <file_dir> <result_dir>""".format(sys.argv[0]))
-        exit(1)
+        print("""Usage:  {} <fileDir> <resultsDir>""".format(sys.argv[0]))
+        sys.exit(1)
     parser = argparse.ArgumentParser(description="analysis for 5mC files")
-    parser.add_argument("FILE_dir", type=str, help="directory folder contains source files")
-    parser.add_argument("RESULT_dir", type=str, help="directory folder to store result files")
-    parser.add_argument('clean', action='store_true', help="""Would you like to remove all files ouput files not used further on?""")
+    parser.add_argument("fileDir", type=str, help="directory folder contains source files")
+    parser.add_argument("resultsDir", type=str, help="directory folder to store resultsDir files")
+    parser.add_argument('clean', type=str, help="Would you like to remove all files output files not used further on?")
     args = parser.parse_args()
-    file = args.FILE_dir
-    result = args.RESULT_dir
-    clean = args.clean
-    stepAnalyzing(file, result, clean)
+    fileDir = args.fileDir
+    resultsDir = args.resultsDir
+    cleanStr = args.clean
+    if cleanStr == 'True':
+        clean = True
+    else:
+        clean = False
+    stepAnalyzing(fileDir, resultsDir, clean)
     
     
